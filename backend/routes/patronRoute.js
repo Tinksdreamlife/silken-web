5138027601d2731097721e654179f918015848a3
Tinksdreamlife <tinksdreamlife@gmail.com>
2025-06-10 13:01:34 -0400

    Drafted patronRoute dot js file

---
+++ backend/routes/patronRoute.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router;
+const patronsCtrl = require('../controllers/patronsCtrl');
+
+// GET /api/patrons (all patrons)
+router.get('/', patronsCtrl.index);
+
+// GET /api/patrons/:id (get a single patron by their ID)
+router.get('/:id', patronsCtrl.show);
+
+// POST /api/patrons (CREATE a new patron)
+router.post('/', patronsCtrl.create);
+
+// PUT /api/patrons/:id (UPDATE an existing patron)
+router.put('/', patronsCtrl.update);
+
+// DELETE /api/patrons/:id (DELETE a patron)
+router.delete('/:id', patronsCtrl.delete)
+
+module.exports = router;
