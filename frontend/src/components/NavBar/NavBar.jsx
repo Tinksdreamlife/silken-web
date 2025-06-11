c9b07f5d3a78c67fa36a000e755eaed114fb840c
Tinksdreamlife <tinksdreamlife@gmail.com>
2025-06-11 10:58:14 -0400

    fixed issue where home was showing twice in navigation after login

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -17,8 +17,6 @@ export default function NavBar({ user, setUser }) {
       &nbsp; | &nbsp;
       {user ? (
         <>
-          <NavLink to="/">Home</NavLink>
-          &nbsp; | &nbsp;
           <NavLink to="/posts" end>
             Post List
           </NavLink>
