0f818ca964d5541d5637bc1eec1623a147250aa5
Tinksdreamlife <tinksdreamlife@gmail.com>
2025-06-17 15:34:08 -0400

    Redrafted Home Page to be new guest Home Page before login with dedicated css and updated index css for stretch also media responsive

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,24 @@
+import './HomePage.css'
+import { Link } from 'react-router-dom';
+
+
 export default function HomePage() {
-  return <h1>Home Page</h1>
+  return (
+  <div className="welcome-page">
+    <div className="overlay">
+      <h1>Welcome to the Silken Web</h1>
+      <p>
+        The first and only CRM made for adult entertainers to track patrons across platforms and venues.
+        Whether you met them in a club, or slid into their DMs, keep every strand of your hustle organized.
+      </p>
+      <p>
+        Spin your empire - one connection at a time.
+      </p>
+      <div className="welcome-buttons">
+        <Link to="/signup" className="btn">Create Account</Link>
+        <Link to="/login" className="btn">Log In</Link>
+      </div>
+    </div>
+  </div>
+  );
 }
